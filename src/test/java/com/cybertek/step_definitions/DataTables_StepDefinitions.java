4c7e14c99af9f213be4994e907c1f2a8766b3919
muhammad shoaib <muhammadshoaib280@gmail.com>
2021-07-24 11:55:22 -0400

    added new

--- src/test/java/com/cybertek/step_definitions/DataTables_StepDefinitions.java
+++ src/test/java/com/cybertek/step_definitions/DataTables_StepDefinitions.java
@@ -1,19 +1,51 @@
 package com.cybertek.step_definitions;
 
+import com.cybertek.pages.DropdownsPage;
 import com.cybertek.pages.LibraryLoginPage;
 import com.cybertek.utilities.ConfigurationReader;
 import com.cybertek.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 public class DataTables_StepDefinitions {
 
     LibraryLoginPage loginPage = new LibraryLoginPage();
+    DropdownsPage dropdownsPage = new DropdownsPage();
+
+    @Given("User is on the dropdowns page of practice tool")
+    public void user_is_on_the_dropdowns_page_of_practice_tool() {
+        Driver.getDriver().get("http://practice.cybertekschool.com/dropdown");
+    }
+    @Then("User should see below info in month dropdown")
+    public void user_should_see_below_info_in_month_dropdown(List<String> expectedList) {
+
+        //1- Get the dropdown as a select object
+        Select monthDropdown = new Select(dropdownsPage.month);
+
+        //2- Get all the options from the dropdown and store inside of a List
+        List<WebElement> actualMonthsAsWebElement = monthDropdown.getOptions();
+
+        //3- Convert the actualMonths from List<WebElement> to List<String>
+        List<String> actualMonthsAsString = new ArrayList<>();
+
+        for (WebElement each : actualMonthsAsWebElement) {
+
+            actualMonthsAsString.add(each.getText());
+
+        }
+        Assert.assertEquals(expectedList,actualMonthsAsString);
+
+    }
+
+
 
     @Given("user is on the login page of library app")
     public void user_is_on_the_login_page_of_library_app() {
@@ -32,13 +64,15 @@ public class DataTables_StepDefinitions {
         loginPage.emailInput.sendKeys(username);
         loginPage.passwordInput.sendKeys(loginInfo.get("password"));
 
-
         loginPage.signIn.click();
 
     }
-    @Then("user should see title is something")
+    @Then("user should see title is Library")
     public void user_should_see_title_is_something() {
-        System.out.println("title verification");
+
+        String actualTitle = Driver.getDriver().getTitle();
+        String expectedTitle = "Library";
+
     }
 
 
@@ -48,8 +82,8 @@ public class DataTables_StepDefinitions {
         System.out.println("listOfFruits.size() = " + listOfFruits.size());
         System.out.println("listOfFruits = " + listOfFruits);
 
-
     }
+
 
 
 }
--- src/test/java/com/cybertek/step_definitions/Hooks.java
+++ src/test/java/com/cybertek/step_definitions/Hooks.java
@@ -6,29 +6,46 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
 public class Hooks {
-    @Before
-    public void setupScenrio(){
-        System.out.println("setting up browser with further details...");
+
+    //Import from io.cucumber.java not from junit
+    @Before (value = "@login", order = 1)
+    public void setupLoginScenario(){
+        System.out.println("BEFORE--Setting up browser with further details...");
+    }
+
+    @Before ("@db")
+    public void setupScenario(){
+        System.out.println("BEFORE--Setting up browser with further details...");
     }
 
     @After
     public void tearDownScenario(Scenario scenario){
 
-        if (scenario.isFailed()) {
-            byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+        //IF MY SCENARIO FAILS
+        // TAKE A SCREENSHOT
+
+
+        //scenario.isFailed() --> if scenario fails : returns true
+
+        if (scenario.isFailed()){
+
+            byte [] screenshot = ((TakesScreenshot)Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
             scenario.attach(screenshot, "image/png", scenario.getName());
+
         }
 
-        System.out.println("--Teardown steps are being applied...");
-        // Driver.closeDriver();
-
+        System.out.println("AFTER--Teardown steps are being applied...");
+        Driver.closeDriver();
     }
+
     @BeforeStep
     public void setupStep(){
         System.out.println("----setup applying for each step");
     }
+
     @AfterStep
     public void afterStep(){
-        System.out.println("---teardown applying for each step");
+        System.out.println("----teardown applying for each step");
     }
+
 }
